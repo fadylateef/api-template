18592c78b070bb49e399ef937fcdca1a4f90ac2d
Fady Lateef <fady.lateef.shoukry@gmail.com>
2021-04-09 19:28:00 +0300

    d

--- Sources/App/boot.swift
+++ Sources/App/boot.swift
@@ -14,27 +14,27 @@ public func boot(_ app: Application) throws {
             guard let series_id = split.first else { return }
             let episode_id = split[1]
             guard let episode_link = split.last else { return }
-            ws.send(text: "1/6 Downloading ... ")
+            ws.send(text: "\(series_id),1/6 Downloading ... ")
             do {
                 let videoName = "\(series_id)_\(episode_id).mp4"
                 let imageName = "\(series_id)_\(episode_id).jpg"
                 let hlsName = "\(series_id)_\(episode_id).m3u8"
                 try shellOut(to: "youtube-dl -f mpd-3 \(episode_link) -o /videos/\(series_id)/\(videoName)")
-                ws.send(text: "2/6 Duration ...")
+                ws.send(text: "\(series_id),2/6 Duration ...")
                 let episode_length = try shellOut(to: "mediainfo --Inform=\"General;%Duration%\" /videos/\(series_id)/\(videoName)")
                 guard let epi_length = Int(episode_length) else { return }
                 let episode_duration = epi_length / 60000
-                ws.send(text: "3/6 TS Screenshot ...")
+                ws.send(text: "\(series_id),3/6 TS Screenshot ...")
                 try shellOut(to: "ffmpeg -ss 00:5:00 -i /videos/\(series_id)/\(videoName) -vframes 1 -q:v 20 /images/\(series_id)/\(imageName)")
-                ws.send(text: "4/6 TS Conversion ...")
+                ws.send(text: "\(series_id),4/6 TS Conversion ...")
                 try shellOut(to: "ffmpeg -i /videos/\(series_id)/\(videoName) -codec: copy -start_number 0 -hls_time 10 -hls_list_size 0 -f hls /videos/\(series_id)/\(hlsName)")
-                ws.send(text: "5/6 Saving to DB ...")
+                ws.send(text: "\(series_id),5/6 Saving to DB ...")
                 let newEpi = Episode(filename: hlsName, seriesID: Int(series_id)!, thumbnail: imageName, duration: episode_duration, order: Int(episode_id)!)
                 guard let newEpisode = try? newEpi.save(on: req) else { return }
-                ws.send(text: "6/6 Done ✅")
+                ws.send(text: "\(series_id),6/6 Done ✅")
             }catch {
                 print(error)
-                ws.send(text: "⛔️ Error : \n \(error)")
+                ws.send(text: "\(series_id),⛔️ Error : \n \(error)")
                 return
             }
             
