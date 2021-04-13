c75cd43e4aba6615a1e9351f4c08a8dd566c3038
Fady Lateef <fady.lateef.shoukry@gmail.com>
2021-04-13 04:03:55 +0300

    d

--- Package.swift
+++ Package.swift
@@ -14,10 +14,11 @@ let package = Package(
                  from: "3.0.0"),
         // ShellOut Package
         .package(url: "https://github.com/JohnSundell/ShellOut.git", from: "2.0.0"),
-        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0")
+        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0"),
+        .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "1.7.4")
     ],
     targets: [
-        .target(name: "App", dependencies: ["FluentMySQL", "Vapor","ShellOut","Leaf"]),
+        .target(name: "App", dependencies: ["FluentMySQL", "Vapor","ShellOut","Leaf","SwiftSoup"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
