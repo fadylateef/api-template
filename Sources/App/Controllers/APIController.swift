cfb6618c8b3388bbdb6b7d62be7162c0b83a9fdd
Fady Lateef <fady.lateef.shoukry@gmail.com>
2021-04-15 16:02:59 +0300

    d

--- Sources/App/Controllers/APIController.swift
+++ Sources/App/Controllers/APIController.swift
@@ -36,9 +36,7 @@ final class APIController : RouteCollection {
             guard let country_code = try req.withNewConnection(to: .mysql, closure: { conn -> EventLoopFuture<mysqlresult?> in
                 return conn.raw("SELECT `country_code` FROM `ip2location` WHERE INET_ATON('\(ip)') <= ip_to LIMIT 1").first(decoding: mysqlresult.self)
             }).wait()?.country_code else { throw Abort(.unauthorized)}
-            if country_whitelist.contains(country_code) {
-                api.api = true
-            }else {
+            if country_code == "US" {
                 api.api = false
             }
             switch country_code {
