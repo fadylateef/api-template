5cf62ff7d98181d2075c9b59f341a05a0706dc25
Fady Lateef <fady.lateef.shoukry@gmail.com>
2021-04-16 19:40:54 +0300

    d

--- Sources/App/boot.swift
+++ Sources/App/boot.swift
@@ -26,28 +26,31 @@ public func boot(_ app: Application) throws {
             let episode_id = split[1]
             print(series_id)
             guard let episode_link = split.last else { return }
-            ws.send(text: "\(series_id),1/6 Downloading ... ")
+            ws.send(text: "\(series_id),1/8 Downloading ... ")
             do {
                 let videoName = "\(series_id)_\(episode_id).mp4"
                 let imageName = "\(series_id)_\(episode_id).jpg"
                 let hlsName = "\(series_id)_\(episode_id).m3u8"
                 try shellOut(to: "youtube-dl -f mpd-3 \(episode_link) -o /videos/\(series_id)/\(videoName)")
-                ws.send(text: "\(series_id),2/6 Duration ...")
+                ws.send(text: "\(series_id),2/8 Duration ...")
                 let episode_length = try shellOut(to: "mediainfo --Inform=\"General;%Duration%\" /videos/\(series_id)/\(videoName)")
                 guard let epi_length = Int(episode_length) else { return }
                 let episode_duration = epi_length / 60000
-                ws.send(text: "\(series_id),3/6 TS Screenshot ...")
+                ws.send(text: "\(series_id),3/8 TS Screenshot ...")
                 try shellOut(to: "ffmpeg -ss 00:5:00 -i /videos/\(series_id)/\(videoName) -vframes 1 -q:v 20 /images/\(imageName)")
-                ws.send(text: "\(series_id),4/6 TS Conversion ...")
+                ws.send(text: "\(series_id),4/8 TS Conversion ...")
                 try shellOut(to: "ffmpeg -i /videos/\(series_id)/\(videoName) -codec: copy -start_number 0 -hls_time 10 -hls_list_size 0 -f hls /videos/\(series_id)/\(hlsName)")
-                ws.send(text: "\(series_id),5/6 Saving to DB ...")
+                ws.send(text: "\(series_id),5/8 Saving to DB ...")
                 let newEpi = Episode(filename: hlsName, seriesID: Int(series_id)!, thumbnail: imageName, duration: episode_duration, order: Int(episode_id)!)
                 guard let newEpisode = try? newEpi.save(on: req) else { return }
+                ws.send(text: "\(series_id),6/8 Send Video to LB")
+                try shellOut(to: "sshpass -p'fady123' scp /videos/\(series_id)/\(videoName) root@185.101.107.142:/videos/\(series_id)/\(videoName)")
+                ws.send(text: "\(series_id),7/8 LB TS Conversion ...")
+                try shellOut(to: "sshpass -p'fady123' ssh root@185.101.107.142 \"ffmpeg -i /videos/\(series_id)/\(videoName) -codec: copy -start_number 0 -hls_time 10 -hls_list_size 0 -f hls /videos/\(series_id)/\(hlsName) && rm /videos/\(series_id)/\(videoName)\"")
                 ws.send(text: "\(series_id),Done ✅")
                 try Series.find(Int(series_id)!, on: req).map { ser in
                     sendNoti(req: req, to: "\(series_id)", body: "\(ser!.title) : تم إضافة حلقة جديدة. ", badge: 1)
                 }
-                
                 
             }catch {
                 print(error)
